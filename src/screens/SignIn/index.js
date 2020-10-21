66ad6ab414002c5d037c18ee4640974a81a6acfe
GILBERTO JUNIOR DOS SANTOS <giljrsantos@gmail.com>
2020-10-21 19:51:59 -0300

    First Commit - Barber

---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { 
+    Container,
+    InputArea,
+    CustomButton,
+    CustomButtonText,
+    SignMessageButton,
+    SignMessageButtonText,
+    SignMessageButtonTextBold
+} from './styles';
+
+import BarberLogo from '../../assets/barber.svg';
+
+export default () => {
+    return (
+        <Container>
+            <BarberLogo width="100%" height="160" />
+
+            <InputArea>
+
+                <CustomButton>
+                    <CustomButtonText>LOGIN</CustomButtonText>
+                </CustomButton>
+            </InputArea>
+
+            <SignMessageButton>
+                <SignMessageButtonText>Ainda não possui uma conta?</SignMessageButtonText>
+                <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+            </SignMessageButton>
+        </Container>
+    );
+}
